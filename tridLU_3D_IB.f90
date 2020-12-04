5dbc730df802c7d001a2717dda2cc9ab22ff371c
Mel <mja77@cam.ac.uk>
2020-12-04 15:01:05 +0000

    Solid/black points only commented out f_list

--- tridLU_3D_IB.f90
+++ tridLU_3D_IB.f90
@@ -255,26 +255,27 @@ subroutine immersed_boundaries_U(u,rhsu,Lu,grid,myid)
    enddo
 
 !   write(*,*) 'myid', myid
-   do ilist = 1,nlist_ib_f(grid)
-     i = f_list_ib(1,ilist,grid)
-     k = f_list_ib(2,ilist,grid)
-     j = f_list_ib(3,ilist,grid)
-     i2= f_list_ib(4,ilist,grid)
-     k2= f_list_ib(5,ilist,grid)
-     j2= f_list_ib(6,ilist,grid)
-     i3= f_list_ib(7,ilist,grid)
-     k3= f_list_ib(8,ilist,grid)
-     j3= f_list_ib(9,ilist,grid)
+!  SOLID/BLACK POINTS ONLY 
+!   do ilist = 1,nlist_ib_f(grid)
+!     i = f_list_ib(1,ilist,grid)
+!     k = f_list_ib(2,ilist,grid)
+!     j = f_list_ib(3,ilist,grid)
+!     i2= f_list_ib(4,ilist,grid)
+!     k2= f_list_ib(5,ilist,grid)
+!     j2= f_list_ib(6,ilist,grid)
+!     i3= f_list_ib(7,ilist,grid)
+!     k3= f_list_ib(8,ilist,grid)
+!     j3= f_list_ib(9,ilist,grid)
      
-     w1= w_list_ib(1,ilist,grid)
-     w2= w_list_ib(2,ilist,grid)
-     w3= w_list_ib(3,ilist,grid)
+!     w1= w_list_ib(1,ilist,grid)
+!     w2= w_list_ib(2,ilist,grid)
+!     w3= w_list_ib(3,ilist,grid)
 
-     v_f = w1*0d0 + w2*u1PL(i2,k2,j2) + w3*u1PL(i3,k3,j3)
+!     v_f = w1*0d0 + w2*u1PL(i2,k2,j2) + w3*u1PL(i3,k3,j3)
 
 !     write(*,*) i , k, j, ';'
-     rhsuIB(i,k,j) = v_f - beta*LuIB(i,k,j)
-   enddo
+!     rhsuIB(i,k,j) = v_f - beta*LuIB(i,k,j)
+!   enddo
 
    do j = nyuIB1(myid),nyuIB2(myid)
      call phys_to_four_du(rhsuIB(1,1,j),bandPL(myid))
@@ -332,26 +333,26 @@ subroutine immersed_boundaries_V(u,rhsu,Lu,grid,myid)
 
      rhsuIB(i,k,j) = -beta*LuIB(i,k,j)
    enddo
+! SOLID/BLACK POINTS ONLY
+!   do ilist = 1,nlist_ib_f(grid)
+!     i = f_list_ib(1,ilist,grid)
+!     k = f_list_ib(2,ilist,grid)
+!     j = f_list_ib(3,ilist,grid)
+!     i2= f_list_ib(4,ilist,grid)
+!     k2= f_list_ib(5,ilist,grid)
+!     j2= f_list_ib(6,ilist,grid)
+!     i3= f_list_ib(7,ilist,grid)
+!     k3= f_list_ib(8,ilist,grid)
+!     j3= f_list_ib(9,ilist,grid)
 
-   do ilist = 1,nlist_ib_f(grid)
-     i = f_list_ib(1,ilist,grid)
-     k = f_list_ib(2,ilist,grid)
-     j = f_list_ib(3,ilist,grid)
-     i2= f_list_ib(4,ilist,grid)
-     k2= f_list_ib(5,ilist,grid)
-     j2= f_list_ib(6,ilist,grid)
-     i3= f_list_ib(7,ilist,grid)
-     k3= f_list_ib(8,ilist,grid)
-     j3= f_list_ib(9,ilist,grid)
+!     w1= w_list_ib(1,ilist,grid)
+!     w2= w_list_ib(2,ilist,grid)
+!     w3= w_list_ib(3,ilist,grid)
 
-     w1= w_list_ib(1,ilist,grid)
-     w2= w_list_ib(2,ilist,grid)
-     w3= w_list_ib(3,ilist,grid)
+!     v_f = w1*0d0 + w2*u2PL(i2,k2,j2) + w3*u2PL(i3,k3,j3)    
 
-     v_f = w1*0d0 + w2*u2PL(i2,k2,j2) + w3*u2PL(i3,k3,j3)    
-
-     rhsuIB(i,k,j) = v_f - beta*LuIB(i,k,j)
-   enddo
+!     rhsuIB(i,k,j) = v_f - beta*LuIB(i,k,j)
+!   enddo
 
    do j = nyvIB1(myid),nyvIB2(myid)
      call phys_to_four_du(rhsuIB(1,1,j),bandPL(myid))
